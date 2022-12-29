bb5acd4f825281767db6cd55b0c1c9851fc91d5f
Htet-Aung-Shane <ha.shane@kis-mm.com>
2022-12-29 15:30:05 +0630

    store

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DPS's Address Search</title>
+    <!-- Favicons -->
+    <link href="{{ URL::to('/') }}/images/logo.png" rel="icon">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
